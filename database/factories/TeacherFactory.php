48b57fbb1a1c2ae6fdfc4e4975db7d66b0c04529
mihiranchathuranga <154015E@uom.lk>
2020-10-15 05:36:57 +0530

    create Student,Teacher ,ClassRoom Models ,controllers and migrations.And also create the api routes

---
+++ database/factories/TeacherFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Model\Teacher::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::Resources('student','StudentController');
+
+Route::Resources('teacher','TeacherController');
+
+Route::Resources('classroom','ClassRoomController');
