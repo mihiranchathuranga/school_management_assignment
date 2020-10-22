eba04725f33acacfe6d97c69aff882db59a5ab59
mihiranchathuranga <154015E@uom.lk>
2020-10-22 18:46:25 +0530

    create new seeder

--- database/seeds/StudentsTableSeeder.php
+++ database/seeds/StudentsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
 
 class StudentsTableSeeder extends Seeder
 {
@@ -10,7 +12,22 @@ class StudentsTableSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        //
+    {   
+    	$faker = \Faker\Factory::create('App\Student');
+        $gender = $faker->randomElements(['male','female'])[0];
+
+        //Faker\Factory::create('App\Student');
+
+
+        for($i = 1 ; $i <= 10 ; $i++){
+        DB::table('students')->insert([
+        	'first_name' => $faker->firstName($gender),
+        	'last_name'  => $faker->lastName,
+        	'gender'     => $gender,
+        	'joined_year'=> $faker->year($max = 'now'),
+        	'created_at' => \Carbon\Carbon::now(),
+        	'updated_at' => \Carbon\Carbon::now(),
+        ]);
+      }
     }
 }
--- database/seeds/TeachersTableSeeder.php
+++ database/seeds/TeachersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
 
 class TeachersTableSeeder extends Seeder
 {
@@ -12,10 +14,14 @@ class TeachersTableSeeder extends Seeder
     public function run()
     {
         DB::table('teachers')->insert([
-            'name' => 'Pushpa']);
+            'name'       => 'Pushpa',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')]);
 
         DB::table('teachers')->insert([
-            'name' => 'Ramya']);
+            'name' => 'Ramya',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')]);
 
 
 
